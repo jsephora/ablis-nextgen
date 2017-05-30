48f338db1913471303cc2b0ea9425f644dafe6a0
Joey Sephora <jsephora@users.noreply.github.com>
2017-05-30 15:40:23 +1000

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,7 @@
 $(document).ready(function () {
+    
+    //--- Load All Results by default --------//
+    $('#searchResultsPanel').load('allresults.html');    
 
     //--- Menu Toggles ---//
 
@@ -11,19 +14,23 @@ $(document).ready(function () {
         e.preventDefault();
         $("#wrapper").toggleClass("toggled-right");
     });
-
-
-    //--- Loading Service Details ---//
-
-    $('button').click(function (e) {
-
-        var buttonId = this.id;
-        var objectFileName = '<object data="' + buttonId + '.html" />';
-
+    
+    //-- Loading Search Results --//
+    
+    $('#showAlcohol').click(function (e) {
         e.preventDefault();
-        $('#serviceDetailsPanel').html(objectFileName);
+        $('#searchResultsPanel').load('alcohol.html');
     });
-
+    
+    $('#showEmployment').click(function (e) {
+        e.preventDefault();
+        $('#searchResultsPanel').load('employment.html');
+    });
+    
+    $('#showAll').click(function (e) {
+        e.preventDefault();
+        $('#searchResultsPanel').load('allresults.html');
+    });
 
 
     //--- Prevent main window scroll when scrolling Service Details ---//
@@ -35,38 +42,21 @@ $(document).ready(function () {
         this.scrollTop += (delta < 0 ? 1 : -1) * 30;
         e.preventDefault();
     });
-
-//    //--- Add/Remove to pack
-//    $('#add').click(function() {
-//        var addTitle = $(this).next('div').data();
-//        var title = addTitle.titel;
-//		var button = "<button class='delete'>remove</button>";
-//        
-//        $('ul.sidebar-nav-right').append("<li>" + title + button + "</li>");
-//    });
-//    
-//    $(document).on("click",".delete",function() {
-//        $(this).parent('li').remove();
-//        $(this).remove();
-//    });
     
-    $('.add').click(function() {
-        
-        var $h3 = $(event.target).closest('.serviceTile').find('h3');
-        var $title = $($h3).text();
-        var delButton = "<button class='delete'>remove</button>";
-        
-        $('ul.sidebar-nav-right').append('<li>' + $title + delButton + '</li>');
-//        $('.add').prop('disabled', true);
-        
-    });
+    //--- Add all to pack -----------------
     
-    $(document).on("click",".delete",function() {
-        $(this).parent('li').remove();
-        $(this).remove();  
-
-    });
-
 
 });
+
+
+
+
+
+
+
+
+
+
+
+
 
