0c00fe6ae0016d0ebf3b56bee0fcd85a9e29c05c
Joey Sephora <jsephora@users.noreply.github.com>
2017-06-05 09:42:06 +1000

    Updated a ton of things

--- js/custom.js
+++ js/custom.js
@@ -1,62 +1,46 @@
 $(document).ready(function () {
-    
+
     //--- Load All Results by default --------//
-    $('#searchResultsPanel').load('allresults.html');    
+    $('#searchResultsPanel').load('allresults.html');
 
     //--- Menu Toggles ---//
 
     $("#menu-toggle-left").click(function (e) {
         e.preventDefault();
-        $("#wrapper").toggleClass("toggled-left");
+        $("#content").toggleClass("toggled-left");
     });
 
     $("#menu-toggle-right").click(function (e) {
         e.preventDefault();
-        $("#wrapper").toggleClass("toggled-right");
+        $("#content").toggleClass("toggled-right");
     });
-    
+
     //-- Loading Search Results --//
-    
+
+    $('.categoryTitle').click(function (e) {
+        var categoryHREF = $(this).attr('data');
+        var categoryID = $(this).attr('id');
+        
+            e.preventDefault();
+            $('#searchResultsPanel').load(categoryHREF);
+        
+    });
+
     $('#showAlcohol').click(function (e) {
         e.preventDefault();
         $('#searchResultsPanel').load('alcohol.html');
     });
-    
+
     $('#showEmployment').click(function (e) {
         e.preventDefault();
         $('#searchResultsPanel').load('employment.html');
     });
-    
+
     $('#showAll').click(function (e) {
         e.preventDefault();
         $('#searchResultsPanel').load('allresults.html');
     });
-
-
-    //--- Prevent main window scroll when scrolling Service Details ---//
-    $('object').on('mousewheel DOMMouseScroll', function (e) {
-
-        var e0 = e.originalEvent;
-        var delta = e0.wheelDelta || -e0.detail;
-
-        this.scrollTop += (delta < 0 ? 1 : -1) * 30;
-        e.preventDefault();
-    });
-    
-    //--- Add all to pack -----------------
-    
-
 });
-
-
-
-
-
-
-
-
-
-
 
 
 
