6f6753cf7b721296a07b02204a78f562eac4a9b7
Joey Sephora <jsephora@users.noreply.github.com>
2017-06-05 17:44:11 +1000

    sidebar toggles, add all to pack, remove, styling

--- js/custom.js
+++ js/custom.js
@@ -8,18 +8,19 @@ $(document).ready(function () {
     $("#menu-toggle-left").click(function (e) {
         e.preventDefault();
         $("#content").toggleClass("toggled-left");
+        $("i").toggleClass('fa-chevron-right fa-chevron-left');
     });
 
     $("#menu-toggle-right").click(function (e) {
         e.preventDefault();
         $("#content").toggleClass("toggled-right");
+        $("i").toggleClass('fa-chevron-left fa-chevron-right');
     });
 
     //-- Loading Search Results --//
 
     $('.categoryTitle').click(function (e) {
         var categoryHREF = $(this).attr('data');
-        var categoryID = $(this).attr('id');
         
             e.preventDefault();
             $('#searchResultsPanel').load(categoryHREF);
@@ -40,7 +41,104 @@ $(document).ready(function () {
         e.preventDefault();
         $('#searchResultsPanel').load('allresults.html');
     });
+    
+    
+//--- Add all/Remove all to/from pack--//
+        var added = true;
+    
+    $('#addAll').click(function(e) {
+
+        var $serviceName = $('.serviceTile').find('h3');
+        var $title = $($serviceName).text();
+        var delButton = "<button class='delete'>remove</button>";
+
+        if (added) {
+        e.preventDefault();
+        $('ul.sidebar-nav-right').append('<li>' + $title + delButton + '</li>');
+        
+        $(this).addClass('clicked');
+        }
+    });
+    
+    $('#addAll').click(function() {
+        added = false;
+    });
+    
+//--- remove from pack and reactivate 'Add all to pack' --//
+    $(document).on("click", ".delete", function() {
+        $(this).parent('li').remove();
+        $(this).remove();
+        added = true;
+        $('#addAll').removeClass('clicked');
+    });
+    
+
+
+    
+//--- Open in New Tab --- //
+//    $('#newTab').click(function() {
+//        
+//        $('#serviceDetailsPanel').load
+//    })
+    
+    
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
